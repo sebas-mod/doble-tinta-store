a489650102d9832cf429ecdf6c514082057e0725
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-03-16 23:11:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById("menu-btn")
+const menu = document.getElementById("menu")
+
+menuBtn.addEventListener("click", () => {
+
+menu.classList.toggle("active")
+
+})
